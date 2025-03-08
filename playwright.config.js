267daf45e5435fa56cff2977998db657e6bab993
DasunJayashan <dasunjayashan123@gmail.com>
2025-03-08 22:56:48 +0530

    Third commit

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,12 @@
 //@ts-check
 import { defineConfig, devices } from '@playwright/test';
-import { ALL } from 'dns';
 
 //Read environment variables from file.
 //https://github.com/motdotla/dotenv
 
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, './env/.env.local') });
+dotenv.config({ path: path.resolve(__dirname, './env/.env.qa') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
